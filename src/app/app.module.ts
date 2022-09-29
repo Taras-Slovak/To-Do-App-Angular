ad57cb59124d717960ac17b4930ee0f4934d8010
Taras Slovak <slovak.taras@gmail.com>
2022-09-29 09:05:09 +0300

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { AuthComponent } from './auth/auth/auth.component';
+import { AuthComponent } from './auth/auth.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './components/header/header.component';
 
